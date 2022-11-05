31d936edab5e35bbdc3394df6c127e88cf3b22a8
Jens Langhammer <jens.langhammer@beryju.org>
2022-11-05 16:45:33 +0100

    error if no command given in mover

--- mover/entrypoint.sh
+++ mover/entrypoint.sh
@@ -3,4 +3,7 @@ if [[ $1 == "sync" ]]; then
     rsync -aHA --progress /source/ /dest
 elif [[ $1 == "sleep" ]]; then
     sleep infinity
+else
+    echo "No command given. Make sure to use the correct mover image for your korb version."
+    exit 1
 fi
